e9f9b5d2eacb477d4d94a5fc4ca3069a05e8d796
Goutham <indugujagan888@gmail.com>
2021-04-16 19:15:33 +0530

    Added Api function to get movie details added movie detail fragment tag in main_nav.xml

--- app/src/main/java/com/example/samplepoc/api/ApiInterface.kt
+++ app/src/main/java/com/example/samplepoc/api/ApiInterface.kt
@@ -1,5 +1,6 @@
 package com.example.samplepoc.api
 
+import com.example.samplepoc.model.MovieDetailResponse
 import com.example.samplepoc.model.MovieListResponse
 import retrofit2.Call
 import retrofit2.http.GET
@@ -8,7 +9,15 @@ import retrofit2.http.Query
 interface ApiInterface {
 
     @GET(".")
-    fun getMovies(@Query("apikey") api_key: String?,
-                  @Query("s") movie_title: String?,
-                  @Query("page") page: Int): Call<MovieListResponse>
+    fun getMovies(
+        @Query("apikey") api_key: String?,
+        @Query("s") movie_title: String?,
+        @Query("page") page: Int
+    ): Call<MovieListResponse>
+
+    @GET(".")
+    fun getMovieDetails(
+        @Query("i") imdb_id: String?,
+        @Query("apikey") api_key: String
+    ): Call<MovieDetailResponse>
 }
